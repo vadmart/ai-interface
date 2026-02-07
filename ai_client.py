a80de7324d6d26f890adb10be46192efdae95f5f
vadmart <z9661744@gmail.com>
2026-02-07 13:21:28 +0200

    implemented chat (need to configure websocket)

---
+++ ai_client.py
@@ -0,0 +1,6 @@
+from ollama import AsyncClient
+
+
+MODEL_NAME = "gpt-oss:120b"
+ollama_client = AsyncClient(host="192.168.1.165:11434")
+
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import logging
+import uuid
+from typing import AsyncIterator
 
+from starlette.testclient import TestClient
+
+from ai_client import ollama_client, MODEL_NAME
+from ollama import Message, ChatResponse
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.websockets import WebSocket, WebSocketDisconnect
@@ -10,6 +16,7 @@ app = FastAPI()
 
 template = Jinja2Templates(directory="templates")
 
+MODEL_MESSAGES: list[Message] = [Message(role="system", content="You're a computer science nerd who answers with a bit of sarcasm")]
 
 @app.get("/")
 def read_root(request: Request):
@@ -22,7 +29,11 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.accept()
         logger.info("WebSocket connection accepted")
         while True:
-            data = await websocket.receive()
+            data = await websocket.receive_json()
+            stream: AsyncIterator[ChatResponse] = await ollama_client.chat(MODEL_NAME, data=data, messages=MODEL_MESSAGES, stream=True)
+            message_id = uuid.uuid4().hex
+            async for part in stream:
+                await websocket.send({"id": message_id, **part.message})
             logger.info(f"WebSocket message: {data}")
     except WebSocketDisconnect:
         logger.info("WebSocket disconnected")
@@ -32,3 +43,11 @@ async def websocket_endpoint(websocket: WebSocket):
             await websocket.close(code=1011)
         except:
             pass
+
+
+def test_websocket(websocket: WebSocket):
+    client = TestClient(app)
+    with client.websocket_connect("/ws") as websocket:
+
+        data = websocket.receive_json()
+        assert data
